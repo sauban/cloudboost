70744855ee6246ccdc09cb8abb0a6a56551efed2
Sauban Lawal <saubanlawal@gmail.com>
2018-06-20 12:14:03 +0100

    Fixing eslint

---
+++ test/.eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+    "globals": {
+        "CB": true
+    },
+    "env": {
+        "browser": true,
+        "es6": true,
+        "node": true
+    },
+    "plugins": ["mocha"],
+    "parser": "babel-eslint",
+    "parserOptions": {
+        "ecmaVersion": 6,
+        "sourceType": "module",
+        "ecmaFeatures": {
+          "modules": true
+        }
+      },
+    "extends": "eslint:recommended",
+    "rules": {
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
